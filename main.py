ab55f6f180063624d078711a0b3a7a0badc15b78
Programmer <programmer@Shoupus-MacBook-Pro.local>
2022-03-27 22:05:31 -0700

    Changed player png

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import time
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('fish.png')
 pygame.display.set_icon(icon)
+
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
 running = True
-colors = [(128, 128, 128), (0, 128, 128), (192, 192, 192), (0, 0, 255), (0, 206, 209)]
 while running:
+
+    screen.fill((0, 206, 209))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    color = random.choice(colors)
-    screen.fill(color)
+
+    player(playerX, playerY)
     pygame.display.update()
-    time.sleep(0.3)
